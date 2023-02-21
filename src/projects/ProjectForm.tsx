906937b04861cf7496061fa521987594edf85a96
Kenta <85465075+hira-kenta@users.noreply.github.com>
2023-02-22 00:09:23 +0900

    バリデーション機能の追加

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -11,10 +11,16 @@ interface ProjectFormProps{
 // 関数内で同一の名前をしようしている箇所などがあった場合に便利。
 const ProjectForm = ({ project: initialProject ,onCancel, onSave }: ProjectFormProps) => {
     const [project, setProject] = useState(initialProject);
-    
+    const [errors, setErrors] = useState({
+        name: '',
+        description: '',
+        budget: '',
+    });
+
     // フォーム送信イベント
     const handleSubmit = (event: SyntheticEvent) =>{
         event.preventDefault();
+        if(!isValid()) return;
         onSave(project);   
     }
 
@@ -43,6 +49,41 @@ const ProjectForm = ({ project: initialProject ,onCancel, onSave }: ProjectFormP
             updatedProject = new Project({...p, ...change});
             return updatedProject;
         })
+
+        // 更新値にエラーが無いか検証する。
+        setErrors(() => validate(updatedProject));
+    }
+
+    //　Formバリデーション機能
+    const validate = (project: Project) => {
+        let errors: any = {name: '', description: '', budget: ''};
+        // Nameチェック
+        if(project.name.length === 0){
+            errors.name = 'Name is required.';
+        }
+        if(project.name.length > 0 && project.name.length < 3){
+            errors.name = 'Name needs to be at least 3 characters.';
+        }
+
+        // Descriptionチェック
+        if(project.description.length === 0)
+        {
+            errors.description = 'Description is required.';
+        }
+        
+        // Budgetチェック
+        if(project.budget === 0){
+            errors.budget = 'Budget must be more than $0.';
+        }
+
+        return errors;
+    }
+    const isValid = () => {
+        return (
+            errors.name.length === 0 &&
+            errors.description.length === 0 &&
+            errors.budget.length === 0
+        );
     }
 
     return (
@@ -58,6 +99,11 @@ const ProjectForm = ({ project: initialProject ,onCancel, onSave }: ProjectFormP
                 value={project.name}
                 onChange={handleChange}
             />
+            {errors.name.length > 0 &&(
+                <div className='card error'>
+                    <p>{errors.name}</p>
+                </div>
+            )}
 
             {/* 説明 */}
             <label htmlFor="description">Project Description</label>
@@ -67,6 +113,11 @@ const ProjectForm = ({ project: initialProject ,onCancel, onSave }: ProjectFormP
                 value={project.description}
                 onChange={handleChange}
             />
+            {errors.description.length > 0 &&(
+                <div className='card error'>
+                    <p>{errors.description}</p>
+                </div>
+            )}
 
             {/* 予算 */}
             <label htmlFor="budget">Project Budget</label>
@@ -77,6 +128,11 @@ const ProjectForm = ({ project: initialProject ,onCancel, onSave }: ProjectFormP
                 value={project.budget}
                 onChange={handleChange}
             />
+            {errors.budget.length > 0 &&(
+                <div className='card error'>
+                    <p>{errors.budget}</p>
+                </div>
+            )}
 
             {/* Active */}
             <label htmlFor="isActive">Active?</label>
